88896779aedac6b0fd12be7841a2ba5de8f78a95
jjhlzn <jinjunhang@hotmail.com>
2017-10-05 15:22:52 +0800

    update

---
+++ checkproduct/getcheckitemresult.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace checkproduct
+{
+    public class GetCheckItemResultRequest : BaseRequest
+    {
+        public string ticetNo
+        {
+            get;
+            set;
+        }
+    }
+
+
+    public partial class getcheckitemresult : BasePage<GetCheckItemRequest>
+    {
+        protected override object handle(GetCheckItemRequest req)
+        {
+            return new
+            {
+                status = 0,
+                errorMessage = "",
+                result = new
+                {
+                    result = "0",
+                    description = "这是一条验货描述",
+                    images = new[]
+                                    {
+                                        new {
+                                            id = "1",
+                                            url = "animal.jpg"
+                                        },
+                                        new
+                                        {
+                                            id = "2",
+                                            url = "test.jpg"
+                                        }
+                                    }
+                }
+            };
+        }
+    }
+}
